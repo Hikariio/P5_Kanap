a6a857e363a9c88b0fff944c2fe2e25104a735d4
Hikari <mustangreiff@hotmail.fr>
2021-12-01 19:13:57 +0100

    Page d'accueil fini

--- front/js/index.js
+++ front/js/index.js
@@ -5,25 +5,31 @@
 // Nomenclature 
 // Camelcase > monNomDeVariable , monNomDeFonction
 // Les noms de function: une action sur quelque chose (ex: pas de stringCleaner ou cleanerString ) Je veux du cleanString
-
-const { response } = require("../../back/app");
+// Le nommage des variables est très important const product => représente 1 produit tandis que const products => représente plusieurs produits un tableau de produits
+// Note a moi même : LES MEILLEURS DEV SONT LES PLUS FEIGNANT
 
 
 function getProducts() {
-    fetch("http://localhost:3000/api/products").then(response => response.json())
+    return fetch("http://localhost:3000/api/products").then(response => response.json());
     // return le fetch
 }
 
 function transformProductToHtml(product) {
-    document.querySelector('#items');
+    document.querySelector('#items').innerHTML += `<a href="./product.html?id=${product._id}">
+    <article>
+      <img src="${product.imageUrl}" alt="${product.altTxt}">
+      <h3 class="productName">${product.name}</h3>
+      <p class="productDescription">${product.description}</p>
+    </article>
+  </a>`;
     // Utilisation des querySelector pour pouvoir formater le produit: objet > HTML
     // <a href="./product.html?id=42">
-    //         <article>
+    //    <article>
     //           <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
     //           <h3 class="productName">Kanap name1</h3>
     //           <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
     //         </article>
-    //       </a>
+    //       </a>     
     // product = {
     //     "colors": ["Blue", "White", "Black"],
     //     "_id": "107fb5b75607497b96722bda5b504926",
---
+++ front/js/product.js
@@ -0,0 +1,3 @@
+// 1- recuperer l'id dans l'URL
+// 2- on va get le produit avec fetch en utilisant l'id dans l'url
+// 3- Afficher le produit
